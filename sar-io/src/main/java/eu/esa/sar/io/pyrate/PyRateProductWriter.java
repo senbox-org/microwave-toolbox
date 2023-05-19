10f3e760b81b007cd60f6fa28ec10ffebb4a7a55
Alex McVittie <alexander.mcvittie@gmail.com>
2023-05-19 16:30:50 -0400

    Removal of old imports and code

--- sar-io/src/main/java/eu/esa/sar/io/pyrate/PyRateProductWriter.java
+++ sar-io/src/main/java/eu/esa/sar/io/pyrate/PyRateProductWriter.java
@@ -3,50 +3,28 @@ package eu.esa.sar.io.pyrate;
 
 import com.bc.ceres.core.ProgressMonitor;
 import eu.esa.sar.io.pyrate.pyrateheader.PyRateHeaderWriter;
-import it.geosolutions.imageio.plugins.tiff.TIFFField;
-import it.geosolutions.imageio.plugins.tiff.TIFFImageWriteParam;
-import it.geosolutions.imageio.plugins.tiff.TIFFTag;
-import it.geosolutions.imageioimpl.plugins.tiff.TIFFIFD;
-import it.geosolutions.imageioimpl.plugins.tiff.TIFFImageMetadata;
-import it.geosolutions.imageioimpl.plugins.tiff.TIFFImageWriter;
 import org.apache.commons.io.FileUtils;
 import org.esa.snap.core.dataio.AbstractProductWriter;
-import org.esa.snap.core.dataio.ProductIO;
 import org.esa.snap.core.dataio.ProductWriter;
 import org.esa.snap.core.dataio.ProductWriterPlugIn;
 
-import org.esa.snap.core.dataio.dimap.DimapHeaderWriter;
 import org.esa.snap.core.datamodel.Band;
 import org.esa.snap.core.datamodel.Product;
 import org.esa.snap.core.datamodel.ProductData;
-import org.esa.snap.core.datamodel.ProductNode;
-import org.esa.snap.core.image.ImageManager;
+
 import org.esa.snap.core.util.ProductUtils;
-import org.esa.snap.core.util.geotiff.GeoTIFF;
-import org.esa.snap.core.util.geotiff.GeoTIFFMetadata;
-import org.esa.snap.dataio.geotiff.GeoTiffBandWriter;
-import org.esa.snap.dataio.geotiff.GeoTiffProductWriter;
+
 import org.esa.snap.dataio.geotiff.GeoTiffProductWriterPlugIn;
-import org.esa.snap.dataio.geotiff.internal.*;
 import org.esa.snap.engine_utilities.datamodel.Unit;
 
-import javax.imageio.*;
-import javax.imageio.stream.FileImageOutputStream;
-import javax.imageio.stream.ImageOutputStream;
-import javax.media.jai.operator.FormatDescriptor;
-import java.awt.color.ColorSpace;
-import java.awt.image.*;
-import java.awt.image.renderable.ParameterBlock;
 import java.io.File;
 import java.io.IOException;
-import java.io.StringWriter;
 import java.text.ParseException;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoField;
 import java.time.temporal.TemporalAccessor;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Locale;
 
 public class PyRateProductWriter extends AbstractProductWriter {
@@ -55,14 +33,8 @@ public class PyRateProductWriter extends AbstractProductWriter {
 
     private HashMap<String, ProductWriter> bandProductWriterHashMap = new HashMap<>();
 
-    private HashMap<String, GeoTiffBandWriter> bandWriterMap = new HashMap<>();
 
     private final String BANNED = "BANNED_DATE";
-
-    private boolean hasStartedWriting = false;
-
-    public static final int PRIVATE_BEAM_TIFF_TAG_NUMBER = 65000;
-
 
 
     /**
@@ -197,17 +169,13 @@ public class PyRateProductWriter extends AbstractProductWriter {
             if(b.getUnit() != null && b.getUnit().contains(unit)){
                 Product productSingleBand = new Product(product.getName(), product.getProductType(), product.getSceneRasterWidth(), product.getSceneRasterHeight());
                 productSingleBand.setSceneGeoCoding(product.getSceneGeoCoding());
-                //b.readRasterDataFully();
-                //ProductUtils.copyBand(b.getName(), product, productSingleBand, true);
+
                 String pyRateName = createPyRateFileName(b.getName(), b.getUnit(), bannedDates);
                 if (pyRateName.equals(BANNED)){
                     continue;
                 }
                 String fileName = new File(new File(processingLocation, "geoTiffs"), pyRateName).getAbsolutePath();
                 productSingleBand.setName(pyRateName);
-                //productSingleBand.getBands()[0].setName(pyRateName);
-
-                //ProductIO.writeProduct(productSingleBand, fileName, format);
 
                 if(format.equals("GeoTIFF")){
                     fileName += ".tif";
@@ -319,131 +287,6 @@ public class PyRateProductWriter extends AbstractProductWriter {
 
     }
 
-
-    /**
-     * From GeoTiffProductWriter with some modifications.
-     *
-     * Writes raster data from the given in-memory source buffer into the data sink specified by the given source band
-     * and region.
-     * <p>
-     * <h3>Source band</h3> The source band is used to identify the data sink in which this method transfers the sample
-     * values given in the source buffer. The method does not modify the pixel data of the given source band at all.
-     * <p>
-     * <h3>Source buffer</h3> The first element of the source buffer corresponds to the given <code>sourceOffsetX</code>
-     * and <code>sourceOffsetY</code> of the source region. These parameters are an offset within the band's raster data
-     * and <b>not</b> an offset within the source buffer.<br> The number of elements in the buffer must be exactly be
-     * <code>sourceWidth * sourceHeight</code>. The pixel values to be writte are considered to be stored in
-     * line-by-line order, so the raster X co-ordinate varies faster than the Y.
-     * <p>
-     * <h3>Source region</h3> The given destination region specified by the <code>sourceOffsetX</code>,
-     * <code>sourceOffsetY</code>, <code>sourceWidth</code> and <code>sourceHeight</code> parameters is given in the
-     * source band's raster co-ordinates. These co-ordinates are identical with the destination raster co-ordinates
-     * since product writers do not support spectral or spatial subsets.
-     *
-     * @param sourceBand   the source band which identifies the data sink to which to write the sample values
-     * @param regionData   the data buffer which provides the sample values to be written
-     * @param regionX      the X-offset in the band's raster co-ordinates
-     * @param regionY      the Y-offset in the band's raster co-ordinates
-     * @param regionWidth  the width of region to be written given in the band's raster co-ordinates
-     * @param regionHeight the height of region to be written given in the band's raster co-ordinates
-     * @param ios          image output stream to determine where on disk this band gets written to.
-     * @param ifd          TIFF IFD tag.
-     * @throws java.io.IOException      if an I/O error occurs
-     * @throws IllegalArgumentException if the number of elements source buffer not equals <code>sourceWidth *
-     *                                  sourceHeight</code> or the source region is out of the band's raster
-     * @see Band#getRasterWidth()
-     * @see Band#getRasterHeight()
-     *
-    public void writeBandRasterData(final Band sourceBand,
-                                    final int regionX,
-                                    final int regionY,
-                                    final int regionWidth,
-                                    final int regionHeight,
-                                    final ProductData regionData,
-                                    ImageOutputStream ios,
-                                    TiffIFD ifd,
-                                    ProgressMonitor pm) throws IOException {
-
-        final int bandDataType = ifd.getBandDataType();
-        final int stripIndex = 0; // The writer writes only single band GeoTIFFs for PyRate.
-        final TiffValue[] offsetValues = ifd.getEntry(TiffTag.STRIP_OFFSETS).getValues();
-        final long stripOffset = ((TiffLong) offsetValues[stripIndex]).getValue();
-        final TiffValue[] bitsPerSampleValues = ifd.getEntry(TiffTag.BITS_PER_SAMPLE).getValues();
-        final long elemSize = ((TiffShort) bitsPerSampleValues[stripIndex]).getValue() / 8;
-        final long sourceWidthBytes = sourceBand.getRasterWidth() * elemSize;
-        final long regionOffsetXInBytes = regionX * elemSize;
-        final long pixelOffset = sourceWidthBytes * regionY + regionOffsetXInBytes;
-        final long startOffset = stripOffset + pixelOffset;
-
-        pm.beginTask("Writing band '" + sourceBand.getName() + "'...", regionHeight);
-        try {
-            for (int y = 0; y < regionHeight; y++) {
-                ios.seek(startOffset + y * sourceWidthBytes);
-                final int stride = y * regionWidth;
-                if (bandDataType == ProductData.TYPE_UINT8) {
-                    final byte[] data = new byte[regionWidth];
-                    for (int x = 0; x < regionWidth; x++) {
-                        data[x] = (byte) regionData.getElemUIntAt(stride + x);
-                    }
-                    ios.write(data);
-                } else if (bandDataType == ProductData.TYPE_INT8) {
-                    final byte[] data = new byte[regionWidth];
-                    for (int x = 0; x < regionWidth; x++) {
-                        data[x] = (byte) regionData.getElemIntAt(stride + x);
-                    }
-                    ios.write(data);
-                } else if (bandDataType == ProductData.TYPE_UINT16) {
-                    final short[] data = new short[regionWidth];
-                    for (int x = 0; x < regionWidth; x++) {
-                        data[x] = (short) regionData.getElemUIntAt(stride + x);
-                    }
-                    ios.writeShorts(data, 0, regionWidth);
-                } else if (bandDataType == ProductData.TYPE_INT16) {
-                    final short[] data = new short[regionWidth];
-                    for (int x = 0; x < regionWidth; x++) {
-                        data[x] = (short) regionData.getElemIntAt(stride + x);
-                    }
-                    ios.writeShorts(data, 0, regionWidth);
-                } else if (bandDataType == ProductData.TYPE_UINT32) {
-                    final int[] data = new int[regionWidth];
-                    for (int x = 0; x < regionWidth; x++) {
-                        data[x] = (int) regionData.getElemUIntAt(stride + x);
-                    }
-                    ios.writeInts(data, 0, regionWidth);
-                } else if (bandDataType == ProductData.TYPE_INT32) {
-                    final int[] data = new int[regionWidth];
-                    for (int x = 0; x < regionWidth; x++) {
-                        data[x] = regionData.getElemIntAt(stride + x);
-                    }
-                    ios.writeInts(data, 0, regionWidth);
-                } else if (bandDataType == ProductData.TYPE_FLOAT32) {
-                    final float[] data = new float[regionWidth];
-                    for (int x = 0; x < regionWidth; x++) {
-                        if (sourceBand.isFlagBand() && ProductData.isUIntType(sourceBand.getDataType())) {
-                            // bits in int and uint are the same. We need int here for the float conversion method.
-                            data[x] = Float.intBitsToFloat(regionData.getElemIntAt(stride + x));
-                        } else {
-                            data[x] = regionData.getElemFloatAt(stride + x);
-                        }
-                    }
-                    ios.writeFloats(data, 0, regionWidth);
-                } else if (bandDataType == ProductData.TYPE_FLOAT64) {
-                    final double[] data = new double[regionWidth];
-                    for (int x = 0; x < regionWidth; x++) {
-                        if (sourceBand.isFlagBand() && ProductData.isUIntType(sourceBand.getDataType())) {
-                            data[x] = Double.longBitsToDouble(regionData.getElemUIntAt(stride + x));
-                        } else {
-                            data[x] = regionData.getElemDoubleAt(stride + x);
-                        }
-                    }
-                    ios.writeDoubles(data, 0, regionWidth);
-                }
-                pm.worked(1);
-            }
-        } finally {
-            pm.done();
-        }
-    }*/
 
 
 }
