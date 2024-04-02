8f00236c0a63dda817098d453480893a2adf0fb9
Luis Veci <luis@skywatch.co>
2024-04-01 20:49:53 -0400

    slight performance improvement

--- sar-op-sentinel1/src/main/java/eu/esa/sar/sentinel1/gpf/etadcorrectors/BaseCorrector.java
+++ sar-op-sentinel1/src/main/java/eu/esa/sar/sentinel1/gpf/etadcorrectors/BaseCorrector.java
@@ -188,17 +188,11 @@ import java.util.Map;
         return new Rectangle(x0, y0, w, h);
     }
 
-    protected double getCorrection(final String layer, final double azimuthTime, final double slantRangeTime,
-                                   final ETADUtils.Burst burst, final Map<String, double[][]>layerCorrectionMap) {
+    protected double getCorrection(final double azimuthTime, final double slantRangeTime,
+                                   final ETADUtils.Burst burst, double[][] layerCorrection) {
 
         if (burst == null) {
             return 0.0;
-        }
-        final String bandName = etadUtils.createBandName(burst.swathID, burst.bIndex, layer);
-        double[][] layerCorrection = layerCorrectionMap.get(bandName);
-        if (layerCorrection == null) {
-            layerCorrection = etadUtils.getLayerCorrectionForCurrentBurst(burst, bandName);
-            layerCorrectionMap.put(bandName, layerCorrection);
         }
         final double i = (azimuthTime - burst.azimuthTimeMin) / burst.gridSamplingAzimuth;
         final double j = (slantRangeTime - burst.rangeTimeMin) / burst.gridSamplingRange;
