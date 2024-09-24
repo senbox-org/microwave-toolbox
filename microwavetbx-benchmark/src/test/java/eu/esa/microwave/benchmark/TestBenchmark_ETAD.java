582b08cc6861eeafea687ded7e6ef697837076f8
Luis Veci <luis@skywatch.co>
2024-09-24 12:22:12 -0400

    add backgeocoding to benchmarks

--- microwavetbx-benchmark/src/test/java/eu/esa/microwave/benchmark/TestBenchmark_ETAD.java
+++ microwavetbx-benchmark/src/test/java/eu/esa/microwave/benchmark/TestBenchmark_ETAD.java
@@ -19,6 +19,7 @@ import com.bc.ceres.binding.dom.DefaultDomElement;
 import com.bc.ceres.binding.dom.DomElement;
 import com.bc.ceres.core.ProgressMonitor;
 import eu.esa.sar.orbits.gpf.ApplyOrbitFileOp;
+import eu.esa.sar.sentinel1.gpf.BackGeocodingOp;
 import eu.esa.sar.sentinel1.gpf.S1ETADCorrectionOp;
 import eu.esa.sar.sentinel1.gpf.TOPSARSplitOp;
 import org.esa.snap.core.datamodel.Product;
@@ -78,22 +79,24 @@ public class TestBenchmark_ETAD extends BaseBenchmarks {
         etad(slcInSAR1, etadInSAR1, ProcessMode.SPLIT_ORBIT_ETAD, WriteMode.GRAPH);
     }
 
+    // Split -> ApplyOrbit -> ETAD -> BackGeoCoding
+
     @Test
     public void testInSAR_Coregister_ProductIO() throws Exception {
         setName(new Throwable().getStackTrace()[0].getMethodName());
-        coregister(slcInSAR1, slcInSAR2, WriteMode.PRODUCT_IO);
+        coregister(slcInSAR1, etadInSAR1, slcInSAR2, etadInSAR2, WriteMode.PRODUCT_IO);
     }
 
     @Test
     public void testInSAR_Coregister_GPF() throws Exception {
         setName(new Throwable().getStackTrace()[0].getMethodName());
-        coregister(slcInSAR1, slcInSAR2, WriteMode.GPF);
+        coregister(slcInSAR1, etadInSAR1, slcInSAR2, etadInSAR2, WriteMode.GPF);
     }
 
     @Test
     public void testInSAR_Coregister_Graph() throws Exception {
         setName(new Throwable().getStackTrace()[0].getMethodName());
-        coregister(slcInSAR1, slcInSAR2, WriteMode.GRAPH);
+        coregister(slcInSAR1, etadInSAR1, slcInSAR2, etadInSAR2, WriteMode.GRAPH);
     }
 
 
@@ -120,30 +123,33 @@ public class TestBenchmark_ETAD extends BaseBenchmarks {
     private void processETAD(final File file1, final File etadFile,
                              final File outputFolder,
                              final ProcessMode processMode, final WriteMode mode) throws Exception {
-        final Product srcProduct = read(file1);
-        Product trgProduct;
+        try(final Product srcProduct = read(file1)) {
+            Product trgProduct;
 
-        TOPSARSplitOp splitOp = new TOPSARSplitOp();
-        splitOp.setSourceProduct(srcProduct);
-        splitOp.setParameter("subswath", "IW1");
-        splitOp.setParameter("selectedPolarisations", "VV");
+            TOPSARSplitOp splitOp = new TOPSARSplitOp();
+            splitOp.setSourceProduct(srcProduct);
+            splitOp.setParameter("subswath", "IW1");
+            splitOp.setParameter("selectedPolarisations", "VV");
 
-        ApplyOrbitFileOp applyOrbitOp = new ApplyOrbitFileOp();
-        applyOrbitOp.setSourceProduct(splitOp.getTargetProduct());
+            ApplyOrbitFileOp applyOrbitOp = new ApplyOrbitFileOp();
+            applyOrbitOp.setSourceProduct(splitOp.getTargetProduct());
 
-        if(processMode.equals(ProcessMode.SPLIT_ORBIT_ETAD)) {
-            S1ETADCorrectionOp etadOp = new S1ETADCorrectionOp();
-            etadOp.setSourceProduct(applyOrbitOp.getTargetProduct());
-            etadOp.setParameter("etadFile", etadFile);
-            trgProduct = etadOp.getTargetProduct();
-        } else {
-            trgProduct = applyOrbitOp.getTargetProduct();
+            if (processMode.equals(ProcessMode.SPLIT_ORBIT_ETAD)) {
+                S1ETADCorrectionOp etadOp = new S1ETADCorrectionOp();
+                etadOp.setSourceProduct(applyOrbitOp.getTargetProduct());
+                etadOp.setParameter("etadFile", etadFile);
+                etadOp.setParameter("resamplingImage", false);
+                etadOp.setParameter("outputPhaseCorrections", true);
+                trgProduct = etadOp.getTargetProduct();
+            } else {
+                trgProduct = applyOrbitOp.getTargetProduct();
+            }
+
+            write(trgProduct, outputFolder, mode);
+
+            trgProduct.dispose();
+            srcProduct.dispose();
         }
-
-        write(trgProduct, outputFolder, mode);
-
-        trgProduct.dispose();
-        srcProduct.dispose();
     }
 
     private void processETADGraph(final File file1, final File etadFile,
@@ -162,6 +168,7 @@ public class TestBenchmark_ETAD extends BaseBenchmarks {
         final DomElement splitParameters = new DefaultDomElement("parameters");
         splitParameters.createChild("subswath").setValue("IW1");
         splitParameters.createChild("selectedPolarisations").setValue("VV");
+        splitNode.setConfiguration(splitParameters);
         graph.addNode(splitNode);
 
         final Node applyOrbitNode = new Node("Apply-Orbit-File", "Apply-Orbit-File");
@@ -173,6 +180,9 @@ public class TestBenchmark_ETAD extends BaseBenchmarks {
             etadNode.addSource(new NodeSource("source", "Apply-Orbit-File"));
             final DomElement etadParameters = new DefaultDomElement("parameters");
             etadParameters.createChild("etadFile").setValue(etadFile.getAbsolutePath());
+            etadParameters.createChild("resamplingImage").setValue("false");
+            etadParameters.createChild("outputPhaseCorrections").setValue("true");
+            etadNode.setConfiguration(etadParameters);
             graph.addNode(etadNode);
         }
 
@@ -193,17 +203,19 @@ public class TestBenchmark_ETAD extends BaseBenchmarks {
         processor.executeGraph(graph, ProgressMonitor.NULL);
     }
 
-    private void coregister(final File srcFile1, final File srcFile2, final WriteMode mode) throws Exception {
+    private void coregister(final File srcFile1, final File etadFile1,
+                            final File srcFile2, final File etadFile2,
+                            final WriteMode mode) throws Exception {
         Benchmark b = new Benchmark(groupName, testName) {
             @Override
             protected void execute() throws Exception {
                 switch (mode) {
                     case PRODUCT_IO:
                     case GPF:
-                        process(srcFile1, srcFile2, outputFolder, mode);
+                        processStack(srcFile1, etadFile1, srcFile2, etadFile2, outputFolder, mode);
                         break;
                     case GRAPH:
-                        processGraph(srcFile1, srcFile2, outputFolder);
+                        processStackGraph(srcFile1, srcFile2, outputFolder);
                         break;
                 }
             }
@@ -211,34 +223,61 @@ public class TestBenchmark_ETAD extends BaseBenchmarks {
         b.run();
     }
 
-    private void process(final File file1, final File file2, final File outputFolder, final WriteMode mode) throws Exception {
-        final Product srcProduct = read(file1);
+    private void processStack(final File srcFile1, final File etadFile1,
+                              final File srcFile2, final File etadFile2,
+                              final File outputFolder, final WriteMode mode) throws Exception {
+        try(final Product srcProduct1 = read(srcFile1)) {
+            try(final Product srcProduct2 = read(srcFile2)) {
 
-        TOPSARSplitOp splitOp = new TOPSARSplitOp();
-        splitOp.setSourceProduct(srcProduct);
-        splitOp.setParameter("subswath", "IW1");
-        splitOp.setParameter("selectedPolarisations", "VV");
+                TOPSARSplitOp splitOp1 = new TOPSARSplitOp();
+                splitOp1.setSourceProduct(srcProduct1);
+                splitOp1.setParameter("subswath", "IW1");
+                splitOp1.setParameter("selectedPolarisations", "VV");
 
-        ApplyOrbitFileOp applyOrbitOp = new ApplyOrbitFileOp();
-        applyOrbitOp.setSourceProduct(splitOp.getTargetProduct());
+                TOPSARSplitOp splitOp2 = new TOPSARSplitOp();
+                splitOp2.setSourceProduct(srcProduct2);
+                splitOp2.setParameter("subswath", "IW1");
+                splitOp2.setParameter("selectedPolarisations", "VV");
 
-        S1ETADCorrectionOp etadOp = new S1ETADCorrectionOp();
-        etadOp.setSourceProduct(applyOrbitOp.getTargetProduct());
-        Product trgProduct = etadOp.getTargetProduct();
+                ApplyOrbitFileOp applyOrbitOp1 = new ApplyOrbitFileOp();
+                applyOrbitOp1.setSourceProduct(splitOp1.getTargetProduct());
 
-        write(trgProduct, outputFolder, mode);
+                ApplyOrbitFileOp applyOrbitOp2 = new ApplyOrbitFileOp();
+                applyOrbitOp2.setSourceProduct(splitOp2.getTargetProduct());
 
-        trgProduct.dispose();
-        srcProduct.dispose();
+                S1ETADCorrectionOp etadOp1 = new S1ETADCorrectionOp();
+                etadOp1.setSourceProduct(applyOrbitOp1.getTargetProduct());
+                etadOp1.setParameter("etadFile", etadFile1);
+                etadOp1.setParameter("resamplingImage", false);
+                etadOp1.setParameter("outputPhaseCorrections", true);
+
+                S1ETADCorrectionOp etadOp2 = new S1ETADCorrectionOp();
+                etadOp2.setSourceProduct(applyOrbitOp2.getTargetProduct());
+                etadOp2.setParameter("etadFile", etadFile2);
+                etadOp2.setParameter("resamplingImage", false);
+                etadOp2.setParameter("outputPhaseCorrections", true);
+
+                BackGeocodingOp backGeoOp = new BackGeocodingOp();
+                backGeoOp.setSourceProducts(etadOp1.getTargetProduct(), etadOp2.getTargetProduct());
+
+                Product trgProduct = backGeoOp.getTargetProduct();
+
+                write(trgProduct, outputFolder, mode);
+
+                trgProduct.dispose();
+                srcProduct1.dispose();
+                srcProduct2.dispose();
+            }
+        }
     }
 
-    private void processGraph(final File file1, final File file2, final File outputFolder) throws Exception {
+    private void processStackGraph(final File srcFile1, final File srcFile2, final File outputFolder) throws Exception {
 
         final Graph graph = new Graph("graph");
 
         final Node readNode = new Node("read", "read");
         final DomElement readParameters = new DefaultDomElement("parameters");
-        readParameters.createChild("file").setValue(file1.getAbsolutePath());
+        readParameters.createChild("file").setValue(srcFile1.getAbsolutePath());
         readNode.setConfiguration(readParameters);
         graph.addNode(readNode);
 
@@ -248,7 +287,7 @@ public class TestBenchmark_ETAD extends BaseBenchmarks {
 
         final Node writeNode = new Node("write", "write");
         final DomElement writeParameters = new DefaultDomElement("parameters");
-        final File outFile = new File(outputFolder, file1.getName());
+        final File outFile = new File(outputFolder, srcFile1.getName());
         writeParameters.createChild("file").setValue(outFile.getAbsolutePath());
         writeNode.setConfiguration(writeParameters);
         writeNode.addSource(new NodeSource("source", "Calibration"));
