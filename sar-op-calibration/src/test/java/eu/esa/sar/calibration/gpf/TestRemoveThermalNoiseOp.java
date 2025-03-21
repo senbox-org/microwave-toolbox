f35320a78905493f428e53a294775939ed2900f7
Jun Lu <jun@skywatch.com>
2025-03-21 14:28:48 -0400

    Added a unit test

--- sar-op-calibration/src/test/java/eu/esa/sar/calibration/gpf/TestRemoveThermalNoiseOp.java
+++ sar-op-calibration/src/test/java/eu/esa/sar/calibration/gpf/TestRemoveThermalNoiseOp.java
@@ -15,12 +15,14 @@
  */
 package eu.esa.sar.calibration.gpf;
 
+import com.bc.ceres.annotation.STTM;
 import com.bc.ceres.core.ProgressMonitor;
 import eu.esa.sar.commons.test.SARTests;
 import eu.esa.sar.commons.test.TestData;
-import org.esa.snap.core.datamodel.Band;
-import org.esa.snap.core.datamodel.Product;
+import org.esa.snap.core.datamodel.*;
 import org.esa.snap.core.gpf.OperatorSpi;
+import org.esa.snap.engine_utilities.datamodel.Unit;
+import org.esa.snap.engine_utilities.gpf.OperatorUtils;
 import org.esa.snap.engine_utilities.gpf.TestProcessor;
 import org.esa.snap.engine_utilities.util.TestUtils;
 import org.junit.Before;
@@ -43,8 +45,8 @@ public class TestRemoveThermalNoiseOp {
     @Before
     public void setUp() {
         // If any of the file does not exist: the test will be ignored
-        assumeTrue(inputFile1 + "not found", inputFile1.exists());
-        assumeTrue(inputFile2 + "not found", inputFile2.exists());
+//        assumeTrue(inputFile1 + "not found", inputFile1.exists());
+//        assumeTrue(inputFile2 + "not found", inputFile2.exists());
     }
 
     static {
@@ -90,6 +92,59 @@ public class TestRemoveThermalNoiseOp {
                 assertEquals(629.0, floatValues[0], 0.0001);
                 assertEquals(2362.0, floatValues[1], 0.0001);
                 assertEquals(6065.0, floatValues[2], 0.0001);
+            }
+        }
+    }
+
+    @Test
+    @STTM("SNAP-3862")
+    public void testProcessingS1_TOPS_SLC() throws Exception {
+        try (final Product sourceProduct = TestUtils.readSourceProduct(new File(
+                "src/test/resources/S1A_IW_SLC__1SDV_20181115T125000_20181115T125015_024599_02B387_2799_split.dim"))) {
+
+            sourceProduct.removeTiePointGrid(sourceProduct.getTiePointGrid("latitude"));
+            sourceProduct.removeTiePointGrid(sourceProduct.getTiePointGrid("longitude"));
+            sourceProduct.removeTiePointGrid(sourceProduct.getTiePointGrid("incident_angle"));
+            sourceProduct.removeTiePointGrid(sourceProduct.getTiePointGrid("elevation_angle"));
+            sourceProduct.removeTiePointGrid(sourceProduct.getTiePointGrid("slant_range_time"));
+            sourceProduct.removeBand(sourceProduct.getBand("i_IW1_VH"));
+            sourceProduct.removeBand(sourceProduct.getBand("q_IW1_VH"));
+            sourceProduct.removeBand(sourceProduct.getBand("Intensity_IW1_VH"));
+
+            final int w = sourceProduct.getSceneRasterWidth();
+            final int h = sourceProduct.getSceneRasterHeight();
+            final TiePointGrid latGrid = new TiePointGrid(OperatorUtils.TPG_LATITUDE, 2, 2, 0.5f, 0.5f,
+                    w, h, new float[]{9.0802f, 9.235484f, 9.41406f, 9.569095f});
+            final TiePointGrid lonGrid = new TiePointGrid(OperatorUtils.TPG_LONGITUDE, 2, 2, 0.5f, 0.5f,
+                    w, h, new float[]{79.81333f, 80.58029f, 79.74476f, 80.51244f});
+            final TiePointGrid incGrid = new TiePointGrid(OperatorUtils.TPG_INCIDENT_ANGLE, 2, 2, 0.5f, 0.5f,
+                    w, h, new float[]{30.814592f, 36.57208f, 30.816704f, 36.573723f});
+            final TiePointGrid eleGrid = new TiePointGrid(OperatorUtils.TPG_ELEVATION_ANGLE, 2, 2, 0.5f, 0.5f,
+                    w, h, new float[]{27.498474f, 32.483097f, 27.500357f, 32.48455f});
+            final TiePointGrid srtGrid = new TiePointGrid(OperatorUtils.TPG_SLANT_RANGE_TIME, 2, 2, 0.5f, 0.5f,
+                    w, h, new float[]{5330301.5f, 5649082.0f, 5330301.5f, 5649082.0f});
+            final TiePointGeoCoding tpGeoCoding = new TiePointGeoCoding(latGrid, lonGrid);
+
+            sourceProduct.addTiePointGrid(latGrid);
+            sourceProduct.addTiePointGrid(lonGrid);
+            sourceProduct.addTiePointGrid(incGrid);
+            sourceProduct.addTiePointGrid(eleGrid);
+            sourceProduct.addTiePointGrid(srtGrid);
+            sourceProduct.setSceneGeoCoding(tpGeoCoding);
+
+            TestUtils.createBand(sourceProduct, "i_IW1_VH", ProductData.TYPE_INT16, Unit.REAL, w, h, true);
+            TestUtils.createBand(sourceProduct, "q_IW1_VH", ProductData.TYPE_INT16, Unit.IMAGINARY, w, h, true);
+
+            try (final Product targetProduct = process(sourceProduct)) {
+                final Band band = targetProduct.getBand("Intensity_IW1_VH");
+                assertNotNull(band);
+
+                final float[] floatValues = new float[8];
+                band.readPixels(0, 0, 4, 2, floatValues, ProgressMonitor.NULL);
+
+                assertEquals(0.0, floatValues[0], 0.0001);
+                assertEquals(0.0, floatValues[1], 0.0001);
+                assertEquals(0.0, floatValues[2], 0.0001);
             }
         }
     }
